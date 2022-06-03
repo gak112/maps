6432bb8caf9ad00d0b60d6f65a0b639263f7d53b
Ajay Kumar <ajay.a02@gmail.com>
2022-06-03 19:16:14 +0530

    map check

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,49 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  @ViewChild('map') mapRef: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
+
+  ionViewDidEnter() {
+
+    this.createMap();
+   // this.getGeoPoints();
+  }
+
+  center = {
+    lat: 16.866186464468438,
+    lng: 79.57450574460671
+  }
+
   constructor() {}
+
+  async createMap() {
+
+    try {
+      this.newMap = await GoogleMap.create({
+        id: 'capacitor-google-maps',
+        element: this.mapRef.nativeElement,
+        apiKey:'AIzaSyADxUOzGHPareBBxxD8Ye_8fVyiNp3YxLM',
+        config: {
+          center: this.center,
+          zoom: 13,
+        },
+        forceCreate: true
+      });
+
+      await this.newMap.enableTrafficLayer(true);
+
+      // if(this.newMap) {
+      //   this.setCamera();
+      // //  this.addMarker();
+      //   this.addHotelMarker();
+      //   this.addUserMarker();
+      // }
+    } catch(ex) {
+      console.log(ex);
+    }
+
+  }
+
 
 }
